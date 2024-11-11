e2ae44131f3ccd21b1f29e2024924b088cfe95a4
Maria Eduarda dos Santos Rosa <maria.e.santosr13@gmail.com>
2024-11-11 14:14:42 -0300

    Add files via upload

---
+++ desafio_final_php2/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "extensao_de_cilios";
+
+// Váriavel de conexão (todos os parâmetros do banco)
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error){
+    echo "Erro de conexão".$conn -> connect_error;
+} else{
+}
+?>
